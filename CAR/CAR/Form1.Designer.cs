6b9e5a22c944678073192a61b53c607e5a2050bb
moal00014 <70176955+moal00014@users.noreply.github.com>
2021-05-12 15:53:42 +0200

    Jag har påbörjat med projektet.

---
+++ CAR/CAR/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿
+namespace CAR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel = new System.Windows.Forms.Panel();
+            this.Line9 = new System.Windows.Forms.PictureBox();
+            this.Line8 = new System.Windows.Forms.PictureBox();
+            this.Line7 = new System.Windows.Forms.PictureBox();
+            this.Line6 = new System.Windows.Forms.PictureBox();
+            this.Line5 = new System.Windows.Forms.PictureBox();
+            this.Line4 = new System.Windows.Forms.PictureBox();
+            this.Line3 = new System.Windows.Forms.PictureBox();
+            this.Line2 = new System.Windows.Forms.PictureBox();
+            this.Line1 = new System.Windows.Forms.PictureBox();
+            this.labelSpeed = new System.Windows.Forms.Label();
+            this.timerAction = new System.Windows.Forms.Timer(this.components);
+            this.Spelare = new System.Windows.Forms.PictureBox();
+            this.panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Line9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Line8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Line7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Line6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Line5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Line4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Line3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Line2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Line1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Spelare)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.panel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel.Controls.Add(this.Spelare);
+            this.panel.Controls.Add(this.Line9);
+            this.panel.Controls.Add(this.Line8);
+            this.panel.Controls.Add(this.Line7);
+            this.panel.Controls.Add(this.Line6);
+            this.panel.Controls.Add(this.Line5);
+            this.panel.Controls.Add(this.Line4);
+            this.panel.Controls.Add(this.Line3);
+            this.panel.Controls.Add(this.Line2);
+            this.panel.Controls.Add(this.Line1);
+            this.panel.Location = new System.Drawing.Point(12, 55);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(606, 608);
+            this.panel.TabIndex = 0;
+            this.panel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_Paint);
+            // 
+            // Line9
+            // 
+            this.Line9.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.Line9.Location = new System.Drawing.Point(488, 446);
+            this.Line9.Name = "Line9";
+            this.Line9.Size = new System.Drawing.Size(11, 132);
+            this.Line9.TabIndex = 8;
+            this.Line9.TabStop = false;
+            // 
+            // Line8
+            // 
+            this.Line8.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.Line8.Location = new System.Drawing.Point(488, 225);
+            this.Line8.Name = "Line8";
+            this.Line8.Size = new System.Drawing.Size(11, 132);
+            this.Line8.TabIndex = 7;
+            this.Line8.TabStop = false;
+            // 
+            // Line7
+            // 
+            this.Line7.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.Line7.Location = new System.Drawing.Point(488, 27);
+            this.Line7.Name = "Line7";
+            this.Line7.Size = new System.Drawing.Size(11, 132);
+            this.Line7.TabIndex = 6;
+            this.Line7.TabStop = false;
+            // 
+            // Line6
+            // 
+            this.Line6.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.Line6.Location = new System.Drawing.Point(296, 446);
+            this.Line6.Name = "Line6";
+            this.Line6.Size = new System.Drawing.Size(11, 132);
+            this.Line6.TabIndex = 5;
+            this.Line6.TabStop = false;
+            // 
+            // Line5
+            // 
+            this.Line5.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.Line5.Location = new System.Drawing.Point(296, 225);
+            this.Line5.Name = "Line5";
+            this.Line5.Size = new System.Drawing.Size(11, 132);
+            this.Line5.TabIndex = 4;
+            this.Line5.TabStop = false;
+            // 
+            // Line4
+            // 
+            this.Line4.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.Line4.Location = new System.Drawing.Point(296, 27);
+            this.Line4.Name = "Line4";
+            this.Line4.Size = new System.Drawing.Size(11, 132);
+            this.Line4.TabIndex = 3;
+            this.Line4.TabStop = false;
+            // 
+            // Line3
+            // 
+            this.Line3.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.Line3.Location = new System.Drawing.Point(113, 446);
+            this.Line3.Name = "Line3";
+            this.Line3.Size = new System.Drawing.Size(11, 132);
+            this.Line3.TabIndex = 2;
+            this.Line3.TabStop = false;
+            // 
+            // Line2
+            // 
+            this.Line2.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.Line2.Location = new System.Drawing.Point(113, 225);
+            this.Line2.Name = "Line2";
+            this.Line2.Size = new System.Drawing.Size(11, 132);
+            this.Line2.TabIndex = 1;
+            this.Line2.TabStop = false;
+            // 
+            // Line1
+            // 
+            this.Line1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.Line1.Location = new System.Drawing.Point(113, 27);
+            this.Line1.Name = "Line1";
+            this.Line1.Size = new System.Drawing.Size(11, 132);
+            this.Line1.TabIndex = 0;
+            this.Line1.TabStop = false;
+            // 
+            // labelSpeed
+            // 
+            this.labelSpeed.AutoSize = true;
+            this.labelSpeed.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.labelSpeed.Location = new System.Drawing.Point(286, 9);
+            this.labelSpeed.Name = "labelSpeed";
+            this.labelSpeed.Size = new System.Drawing.Size(112, 30);
+            this.labelSpeed.TabIndex = 1;
+            this.labelSpeed.Text = "Speed: 0";
+            this.labelSpeed.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timerAction
+            // 
+            this.timerAction.Enabled = true;
+            this.timerAction.Interval = 1;
+            this.timerAction.Tick += new System.EventHandler(this.timerAction_Tick);
+            // 
+            // Spelare
+            // 
+            this.Spelare.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.Spelare.Location = new System.Drawing.Point(279, 521);
+            this.Spelare.Name = "Spelare";
+            this.Spelare.Size = new System.Drawing.Size(45, 80);
+            this.Spelare.TabIndex = 9;
+            this.Spelare.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 667);
+            this.Controls.Add(this.labelSpeed);
+            this.Controls.Add(this.panel);
+            this.Name = "Form1";
+            this.Text = "Car";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.panel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Line9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Line8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Line7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Line6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Line5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Line4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Line3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Line2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Line1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Spelare)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Label labelSpeed;
+        private System.Windows.Forms.PictureBox Line9;
+        private System.Windows.Forms.PictureBox Line8;
+        private System.Windows.Forms.PictureBox Line7;
+        private System.Windows.Forms.PictureBox Line6;
+        private System.Windows.Forms.PictureBox Line5;
+        private System.Windows.Forms.PictureBox Line4;
+        private System.Windows.Forms.PictureBox Line3;
+        private System.Windows.Forms.PictureBox Line2;
+        private System.Windows.Forms.PictureBox Line1;
+        private System.Windows.Forms.Timer timerAction;
+        private System.Windows.Forms.PictureBox Spelare;
+    }
+}
+
---
+++ CAR/CAR/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CAR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        enum Bil { Left, Right, None}
+
+        int speed = 5;
+        Bil bil = Bil.None; 
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void timerAction_Tick(object sender, EventArgs e)
+        {
+            Line1.Top += speed;
+            Line2.Top += speed;
+            Line3.Top += speed;
+            Line4.Top += speed;
+            Line5.Top += speed;
+            Line6.Top += speed;
+            Line7.Top += speed;
+            Line8.Top += speed;
+            Line9.Top += speed;
+
+            if (Line1.Top > panel.Height) Line1.Top = -Line1.Height;
+            if (Line2.Top > panel.Height) Line2.Top = -Line2.Height;
+            if (Line3.Top > panel.Height) Line3.Top = -Line3.Height;
+            if (Line4.Top > panel.Height) Line4.Top = -Line4.Height;
+            if (Line5.Top > panel.Height) Line5.Top = -Line5.Height;
+            if (Line6.Top > panel.Height) Line6.Top = -Line6.Height;
+            if (Line7.Top > panel.Height) Line7.Top = -Line7.Height;
+            if (Line8.Top > panel.Height) Line8.Top = -Line8.Height;
+            if (Line9.Top > panel.Height) Line9.Top = -Line9.Height;
+
+            if (bil == Bil.Left)
+                Spelare.Left -= speed;
+
+            if (bil == Bil.Right)
+                Spelare.Left += speed;
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Left)
+                bil = Bil.Left;
+            else if (e.KeyData == Keys.Right)
+                bil = Bil.Right;
+            else bil = Bil.None; 
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            bil = Bil.None; 
+        }
+    }
+}
